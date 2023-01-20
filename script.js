74ec6e2acf0771236f980dd88ae38862c2d61f43
Albina Zakirova <ZakirovaAlbina@yandex.com>
2023-01-20 14:37:46 +0300

    ПРомежуточно

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-//закрытие и открытие попапов редактирования профиля и добавления поста 
-const editPopup = document.querySelector('#profile-popup'); // попап редактирования профиля
-const addPopup = document.querySelector('#element-popup'); // попап добавления поста
-const editOpenPopupButton = document.querySelectorAll('.profile__edit-button'); // Кнопки для показа попапа редактирования профиля
-const addOpenPopupButton = document.querySelectorAll('.profile__add-button'); // Кнопки для показа попапа добавления поста 
-const closePopupButtons = document.querySelectorAll('.close'); // Кнопка для скрытия попавов
+const editPopup = document.getElementById('profile-popup'); //попап редактирования профиля
+const addPopup = document.getElementById('element-popup'); //попап добавления поста
+const editOpenPopupButton = document.querySelectorAll('.profile__edit-button'); //кнопка открытия попапа редактирования профиля
+const addOpenPopupButton = document.querySelectorAll('.profile__add-button'); //кнопки открытия попапа добавления поста
+const closePopupButtons = document.querySelectorAll('.close'); //кнопки закрытия попапов
 
-const fioInput = document.querySelector('.form__input_name_fio'); // Поле имени в форме редактирования профиля
-const professionInput = document.querySelector('.form__input_name_profession'); // Поле профессии в форме редактирования профиля
-const profileTitle = document.querySelector('.profile__title'); // переменная, в которую сохранится имя
-const profileSubtitle = document.querySelector('.profile__subtitle'); //переменная, в которую сохранится профессия
-const formElementProfile = document.querySelector('#profilefields'); // Форма в окне редактирования профиля
-
-const titleInput = document.querySelector('.form__input_place_name');// Поле названия места в форме добавления поста
-const linkInput = document.querySelector('.form__input_link_photo'); // Поле ссылки в форме добавления поста
-const placeTitle = document.querySelector('.element__info-title'); // переменная, в которую сохранится название места
-const linkPhoto = document.querySelector('.element__photo'); //переменная, в которую сохранится ссылка на фото
-const formElementPlace = document.querySelector('#newplace'); // Форма в окне редактирования профиля
-
-const openPopup = (popup, buttons) => {
+const openPopup = (popup, buttons) => {   //Открыть попап по кнопкам/popup попап, который нужно открыть/buttons кнопки, по которым открывается попап
   buttons.forEach(btn =>
     btn.addEventListener('click', e => {
       e.preventDefault();
       popup.classList.add('popup_opened');
-      fioInput.value = profileTitle.textContent;          //Запись данных из профиля в начальное поле ввода имени
-      professionInput.value = profileSubtitle.textContent; //Запись данных из профиля в начальное поле ввода профессии
-      titleInput.value = placeTitle.textContent;          //Запись данных из профиля в начальное поле ввода названия места
-      linkInput.value = linkPhoto.textContent; //Запись данных из профиля в начальное поле ввода ссылки на фото
     })
   )
 };
@@ -33,7 +16,7 @@ const openPopup = (popup, buttons) => {
 openPopup(editPopup, editOpenPopupButton);
 openPopup(addPopup, addOpenPopupButton);
 
-const closePopup = popup => popup.classList.remove('popup_opened');
+const closePopup = popup => popup.classList.remove('popup_opened');  //закрытие попапа, который нужно закрыть
 
 closePopupButtons.forEach(button =>
   button.addEventListener('click', e =>
@@ -43,29 +26,8 @@ closePopupButtons.forEach(button =>
   )
 );
 
-//отправка формы попапа редактирования профиля
-function profileFormSubmit(evt) {
-    evt.preventDefault(); 
-    profileTitle.textContent = fioInput.value;          // Получите значение полей  из свойства value
-    profileSubtitle.textContent = professionInput.value; //Выберите элементы, куда должны быть вставлены значения полей, Dставьте новые значения с помощью textContent
-    closePopup(editPopup);
-}
-
-formElementProfile.addEventListener('submit', profileFormSubmit); 
-
-//отправка формы попапа добавления поста
-
-function postFormSubmit(evt) {
-  evt.preventDefault(); 
-  placeTitle.textContent = titleInput.value;          // Получите значение полей  из свойства value
-  linkPhoto.textContent = linkInput.value; //Выберите элементы, куда должны быть вставлены значения полей, Dставьте новые значения с помощью textContent
-  closePopup(addPopup);
-}
-
-formElementPlace.addEventListener('submit', postFormSubmit); 
-
-//массив постов
-const initialCards = [
+//массив с готовыми постами
+let initialCards = [
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -90,19 +52,77 @@ const initialCards = [
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
-]; 
+];
 
-//добавление поста из данных формы
 
-const newPost =  (el) => {
-  const elementTemplate = document.querySelector('#element-template').content;
+//добавление нового поста el объект поста {name, link}
+const newPost = el => {
+  const elementTemplate = document.getElementById('element-template').content;
   const element = elementTemplate.querySelector('.element').cloneNode(true);
   const elements = document.querySelector('.elements');
   element.querySelector('.element__info-title').textContent = el.name;
   element.querySelector('.element__photo').src = el.link;
-  
-  elements.prepend(element); 
+  elements.prepend(element);
+
+  //лайк
+  element.querySelector('.element__like').addEventListener('click', evt => { 
+    evt.target.classList.toggle('element__like_active');
+  });
+
+  //удаление поста
+  element.querySelector('.element__trash').addEventListener('click',  () => { 
+    element.remove()
+  });
+
+  //popup фотографий
+  const popapBackground = document.querySelector('.popap_background');
+  const popupPhoto = popapBackground.querySelector('.popup__photo');
+  const popupTitle = popapBackground.querySelector('.popup__info-title');
+  const elementPhoto = document.querySelector('.element__photo');
+  elementPhoto.addEventListener('click', () => {
+    popupPhoto.src = el.link;
+    popupTitle.textContent = el.name;
+    popapBackground.classList.add('popup_opened');
+  });
+
 }
 
-//добавление массива из JS
 initialCards.forEach(newPost);
+
+//обработчик отправки формы добавления поста
+const postFormSubmit = e => {  
+  e.preventDefault();
+
+  const name = document.querySelector('.form__input_place_name').value;
+  const link = document.querySelector('.form__input_link_photo').value;
+  newPost({name, link});
+  initialCards.unshift({name, link});
+
+  closePopup(addPopup);
+};
+
+
+//форма добавления поста
+const postForm = document.getElementById('post-form');
+postForm.addEventListener('submit', postFormSubmit);
+
+//обработчикотправки формы редактирования профиля
+function profileFormSubmit(e) {
+  e.preventDefault();
+
+  const fio = document.querySelector('.form__input_name_fio').value;
+  const profession = document.querySelector('.form__input_name_profession').value;
+
+  const profileTitle = document.querySelector('.profile__title');
+  const profileSubtitle = document.querySelector('.profile__subtitle');
+
+  profileTitle.textContent = fio;
+  profileSubtitle.textContent = profession;
+
+  closePopup(editPopup);
+}
+
+//форма редактирования профиля
+const profileForm = document.getElementById('profile-form');
+profileForm.addEventListener('submit', profileFormSubmit);
+
